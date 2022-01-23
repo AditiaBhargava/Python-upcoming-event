b3dae016f7b4f15665564b4d296cff9ff1e3b3f8
Timur Khutaba <86777573+AditiaBhargava@users.noreply.github.com>
2022-01-23 18:12:57 +0300

    Update get-python-event.py

--- get-python-event.py
+++ get-python-event.py
@@ -2,13 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = 'https://www.python.org/events/python-events'
+url = 'https://www.python.org'
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
 
-headlines2 = soup.find('div', 'most-recent-events').find_all('h3')
+headlines2 = soup.find('div', 'medium-widget event-widget last').find_all('li')
 
-for x in headlines2[:]:  
+for x in headlines2:  
   print(x.text.strip())
+  print()
